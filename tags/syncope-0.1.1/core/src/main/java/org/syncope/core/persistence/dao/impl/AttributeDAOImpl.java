4d0acc59a6839496e89a8de54cb95ece18774a2d
chicchiricco <chicchiricco@27543e50-56f5-532c-ff58-d500ff3352ec>
2010-09-15 13:34:38 +0000

    [maven-release-plugin]  copy for tag syncope-0.1.1

---
+++ tags/syncope-0.1.1/core/src/main/java/org/syncope/core/persistence/dao/impl/AttributeDAOImpl.java
@@ -0,0 +1,68 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.syncope.core.persistence.dao.impl;
+
+import java.util.List;
+import javax.persistence.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.syncope.core.persistence.beans.AbstractAttribute;
+import org.syncope.core.persistence.dao.AttributeDAO;
+
+@Repository
+public class AttributeDAOImpl extends AbstractDAOImpl
+        implements AttributeDAO {
+
+    @Override
+    @Transactional(readOnly = true)
+    public <T extends AbstractAttribute> T find(Long id, Class<T> reference) {
+        return entityManager.find(reference, id);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public <T extends AbstractAttribute> List<T> findAll(Class<T> reference) {
+        Query query = entityManager.createQuery(
+                "SELECT e FROM " + reference.getSimpleName() + " e");
+        return query.getResultList();
+    }
+
+    @Override
+    public <T extends AbstractAttribute> T save(T attribute) {
+        return entityManager.merge(attribute);
+    }
+
+    @Override
+    public <T extends AbstractAttribute> void delete(
+            Long id, Class<T> reference) {
+
+        T attribute = find(id, reference);
+        if (attribute == null) {
+            return;
+        }
+
+        delete(attribute);
+    }
+
+    @Override
+    public <T extends AbstractAttribute> void delete(T attribute) {
+        if (attribute.getOwner() != null) {
+            attribute.getOwner().removeAttribute(attribute);
+        }
+        attribute.getSchema().removeAttribute(attribute);
+
+        entityManager.remove(attribute);
+    }
+}
---
+++ tags/syncope-0.1.1/core/src/main/java/org/syncope/core/persistence/dao/impl/DerivedSchemaDAOImpl.java
@@ -0,0 +1,84 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.syncope.core.persistence.dao.impl;
+
+import java.util.Collections;
+import java.util.List;
+import javax.persistence.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.syncope.core.persistence.beans.AbstractDerivedAttribute;
+import org.syncope.core.persistence.beans.AbstractDerivedSchema;
+import org.syncope.core.persistence.beans.AbstractSchema;
+import org.syncope.core.persistence.dao.DerivedAttributeDAO;
+import org.syncope.core.persistence.dao.DerivedSchemaDAO;
+
+@Repository
+public class DerivedSchemaDAOImpl extends AbstractDAOImpl
+        implements DerivedSchemaDAO {
+
+    @Autowired
+    private DerivedAttributeDAO derivedAttributeDAO;
+
+    @Override
+    @Transactional(readOnly = true)
+    public <T extends AbstractDerivedSchema> T find(String name,
+            Class<T> reference) {
+
+        return entityManager.find(reference, name);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public <T extends AbstractDerivedSchema> List<T> findAll(
+            Class<T> reference) {
+
+        Query query = entityManager.createQuery(
+                "SELECT e FROM " + reference.getSimpleName() + " e");
+        return query.getResultList();
+    }
+
+    @Override
+    public <T extends AbstractDerivedSchema> T save(T derivedSchema) {
+        return entityManager.merge(derivedSchema);
+    }
+
+    @Override
+    public <T extends AbstractDerivedSchema> void delete(String name,
+            Class<T> reference) {
+
+        T derivedSchema = find(name, reference);
+        if (derivedSchema == null) {
+            return;
+        }
+
+        for (AbstractSchema schema : derivedSchema.getSchemas()) {
+            schema.removeDerivedSchema(derivedSchema);
+        }
+        derivedSchema.setSchemas(Collections.EMPTY_SET);
+
+        for (AbstractDerivedAttribute derivedAttribute :
+                derivedSchema.getDerivedAttributes()) {
+
+            derivedAttribute.setDerivedSchema(null);
+            derivedAttributeDAO.delete(derivedAttribute.getId(),
+                    derivedAttribute.getClass());
+        }
+        derivedSchema.setDerivedAttributes(Collections.EMPTY_LIST);
+
+        entityManager.remove(derivedSchema);
+    }
+}
