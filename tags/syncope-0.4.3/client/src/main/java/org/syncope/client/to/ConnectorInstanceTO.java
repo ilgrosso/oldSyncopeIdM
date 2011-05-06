1a8e6cd631e0f4f5de9bfdd81be3741e18ed61ca
chicchiricco <chicchiricco@27543e50-56f5-532c-ff58-d500ff3352ec>
2011-05-06 09:09:42 +0000

    [maven-release-plugin]  copy for tag syncope-0.4.3

---
+++ tags/syncope-0.4.3/client/src/main/java/org/syncope/client/to/ConnectorInstanceTO.java
@@ -0,0 +1,122 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.syncope.client.to;
+
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.Set;
+import org.syncope.client.AbstractBaseBean;
+import org.syncope.types.ConnectorCapability;
+
+public class ConnectorInstanceTO extends AbstractBaseBean {
+
+    private Long id;
+
+    private String bundleName;
+
+    private String version;
+
+    private String connectorName;
+
+    private Set<PropertyTO> configuration;
+
+    private Set<ConnectorCapability> capabilities;
+
+    private String displayName;
+
+    public ConnectorInstanceTO() {
+        configuration = new HashSet<PropertyTO>();
+        capabilities = EnumSet.noneOf(ConnectorCapability.class);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBundleName() {
+        return bundleName;
+    }
+
+    public void setBundleName(String bundlename) {
+        this.bundleName = bundlename;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String bundleversion) {
+        this.version = bundleversion;
+    }
+
+    public Set<PropertyTO> getConfiguration() {
+        return this.configuration;
+    }
+
+    public boolean addConfiguration(PropertyTO property) {
+        return this.configuration.add(property);
+    }
+
+    public boolean removeConfiguration(PropertyTO property) {
+        return this.configuration.remove(property);
+    }
+
+    public void setConfiguration(Set<PropertyTO> configuration) {
+        if (configuration == null || configuration.isEmpty()) {
+            this.configuration.clear();
+        } else {
+            this.configuration = configuration;
+        }
+    }
+
+    public String getConnectorName() {
+        return connectorName;
+    }
+
+    public void setConnectorName(String connectorname) {
+        this.connectorName = connectorname;
+    }
+
+    public boolean addCapability(ConnectorCapability capability) {
+        return capabilities.add(capability);
+    }
+
+    public boolean removeCapability(ConnectorCapability capability) {
+        return capabilities.remove(capability);
+    }
+
+    public Set<ConnectorCapability> getCapabilities() {
+        return capabilities;
+    }
+
+    public void setCapabilities(Set<ConnectorCapability> capabilities) {
+        this.capabilities.clear();
+        if (capabilities != null && !capabilities.isEmpty()) {
+            this.capabilities.addAll(capabilities);
+        }
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+}
---
+++ tags/syncope-0.4.3/core/src/main/java/org/syncope/core/persistence/beans/AbstractAttrValue.java
@@ -0,0 +1,144 @@
+/*
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  under the License.
+ */
+package org.syncope.core.persistence.beans;
+
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+import org.hibernate.validator.constraints.Range;
+import org.syncope.core.persistence.validation.entity.AttrValueCheck;
+
+@MappedSuperclass
+@AttrValueCheck
+public abstract class AbstractAttrValue extends AbstractBaseBean {
+
+    private String stringValue;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateValue;
+
+    @Basic
+    @Range(min = 0, max = 1)
+    private Integer booleanValue;
+
+    private Long longValue;
+
+    private Double doubleValue;
+
+    public abstract Long getId();
+
+    public Boolean getBooleanValue() {
+        if (booleanValue == null) {
+            return null;
+        }
+        return isBooleanAsInteger(booleanValue);
+    }
+
+    public void setBooleanValue(Boolean booleanValue) {
+        if (booleanValue == null) {
+            this.booleanValue = null;
+        } else {
+            this.booleanValue = getBooleanAsInteger(booleanValue);
+        }
+    }
+
+    public Date getDateValue() {
+        return dateValue;
+    }
+
+    public void setDateValue(Date dateValue) {
+        this.dateValue = dateValue;
+    }
+
+    public Double getDoubleValue() {
+        return doubleValue;
+    }
+
+    public void setDoubleValue(Double doubleValue) {
+        this.doubleValue = doubleValue;
+    }
+
+    public Long getLongValue() {
+        return longValue;
+    }
+
+    public void setLongValue(Long longValue) {
+        this.longValue = longValue;
+    }
+
+    public String getStringValue() {
+        return stringValue;
+    }
+
+    public void setStringValue(String stringValue) {
+        this.stringValue = stringValue;
+    }
+
+    public <T> T getValue() {
+        return (T) (booleanValue != null ? getBooleanValue()
+                : (dateValue != null ? getDateValue()
+                : (doubleValue != null ? getDoubleValue()
+                : (longValue != null ? getLongValue()
+                : stringValue))));
+    }
+
+    public String getValueAsString() {
+        String result = null;
+
+        switch (getAttribute().getSchema().getType()) {
+
+            case String:
+                result = getStringValue();
+                break;
+
+            case Boolean:
+                result = getBooleanValue().toString();
+                break;
+
+            case Long:
+                result = getAttribute().getSchema().getFormatter().
+                        format(getLongValue());
+                break;
+
+            case Double:
+                result = getAttribute().getSchema().getFormatter().
+                        format(getDoubleValue());
+                break;
+
+            case Date:
+                result = getAttribute().getSchema().getFormatter().
+                        format(getDateValue());
+
+            default:
+        }
+
+        return result;
+    }
+
+    public abstract <T extends AbstractAttr> T getAttribute();
+
+    public abstract <T extends AbstractAttr> void setAttribute(
+            T attribute);
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.toString(this,
+                ToStringStyle.MULTI_LINE_STYLE);
+    }
+}
