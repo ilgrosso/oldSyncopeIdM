0acf34bc2036cc797e163d1a3e291def009b122f
chicchiricco <chicchiricco@27543e50-56f5-532c-ff58-d500ff3352ec>
2011-12-05 08:49:24 +0000

    Wrong release data

--- tags/syncope-0.6.1/core/src/test/resources/testdb.sql
+++
@@ -1,9 +0,0 @@
-ALTER USER sa SET PASSWORD '${testDbResourcePassword}';
-
-DROP TABLE test IF EXISTS;
-CREATE TABLE test (
-id INTEGER PRIMARY KEY,
-password VARCHAR(255) NOT NULL,
-status VARCHAR(5));
-
-INSERT INTO test VALUES (1, 'password', 'false');
